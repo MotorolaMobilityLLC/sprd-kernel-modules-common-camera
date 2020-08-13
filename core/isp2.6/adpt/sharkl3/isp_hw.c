9b1eb0ea3e0cee3ee41a65bef3d7207577bc1a11
Joey Wu <joey.wu@unisoc.com>
2020-08-13 19:27:42 +0800

    Bug #1388830 bsp: camera: Camera drivers optimization
    [root cause  ] Camera drivers optimization
    [changes     ] isp2.6
    [side effects] na
    [self test   ]: yes
    [download normally]: yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:yes
    [board]:ums512
    [test case]:camera
    [reviewers   ] eddy.wei wuyi.shen
    [change_type ] bringup --> sprd_chip_support
    [tag_product ] common
    
    Camera drivers optimization.
    
    Change-Id: Ib45821ce37bc3b1a09f1d84d46b1605c6a635353
    Signed-off-by: Joey Wu <joey.wu@unisoc.com>

--- core/isp2.6/adpt/sharkl3/isp_hw.c
+++ core/isp2.6/adpt/sharkl3/isp_hw.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019-2020 Unisoc Communications Inc.
+ * Copyright (C) 2020-2021 UNISOC Communications Inc.
  *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
