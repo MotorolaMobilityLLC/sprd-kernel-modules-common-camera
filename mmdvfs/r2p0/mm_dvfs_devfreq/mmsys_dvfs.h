ff87dc239ca655ef5f16a552308ca2b657a34987
mengcheng.wang <mengcheng.wang@unisoc.com>
2021-05-13 15:09:53 +0800

    Bug #1578778 N6pro hw mmdvfs off
    [root cause  ] not use adi interface cfg top reg
    [changes     ] use adi interface cfg top reg
    [side effects] no
    [self test   ]:yes
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:no
    [board]:ums9620
    [test case]:camera
    [reviewers   ] Younger.Yang
    [change_type ] feature_bugfix
    [tag_product ] common
    
    This reverts commit 0ea15ce69d5fbefb16293606d03a704d73463c7a.
    
    Change-Id: I8f82f212ae64c2227f5cba445e43305d2e1d88ca

--- mmdvfs/r2p0/mm_dvfs_devfreq/mmsys_dvfs.h
+++ mmdvfs/r2p0/mm_dvfs_devfreq/mmsys_dvfs.h
@@ -21,7 +21,6 @@
 #include <linux/interrupt.h>
 #include <linux/module.h>
 #include <linux/of.h>
-#include <linux/regmap.h>
 #include <linux/platform_device.h>
 #include "governor.h"
 #include "mmsys_dvfs_comm.h"
@@ -33,7 +32,6 @@ struct mmsys_dvfs {
     struct devfreq *devfreq;
     struct devfreq_event_dev *edev;
     struct ip_dvfs_ops *dvfs_ops;
-	struct regmap *top_apb;
     struct mmsys_dvfs_para mmsys_dvfs_para;
     struct mutex lock;
     struct notifier_block pw_nb;
