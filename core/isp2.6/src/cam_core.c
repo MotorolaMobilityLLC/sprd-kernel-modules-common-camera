8788a648f1fda52eb253100a4bcb0eacb91de2fc
Lu Liu <lu.liu1@unisoc.com>
2022-06-06 11:33:39 +0000

    Bug #1904516 process param_cfg param_status serialy
    [root cause  ] null pointer
    [changes     ] process param_cfg param_status serialy
    [side effects] no
    [self test   ]: yes
    [download normally]: yes
    [power on/off normally]: yes
    [do common repository/branch inspection]: no
    [is there dependence]: no
    [confirm dependent commit]: no
    [board]: ums9620
    [test case]: monkey
    [reviewers   ] yangyang.liu
    [change_type ] feature_bugfix
    [tag_product ] specific
    
    Change-Id: Iccf8c3e664596c97431f1d898f02cee872d16b20
    Signed-off-by: Lu Liu <lu.liu1@unisoc.com>

--- core/isp2.6/src/cam_core.c
+++ core/isp2.6/src/cam_core.c
@@ -7962,7 +7962,8 @@ static long camcore_ioctl(struct file *file, unsigned int cmd,
 	if (atomic_read(&module->state) != CAM_RUNNING
 		|| cmd == SPRD_IMG_IO_STREAM_OFF
 		|| cmd == SPRD_ISP_IO_CFG_PARAM
-		|| cmd == SPRD_IMG_IO_DCAM_SWITCH) {
+		|| cmd == SPRD_IMG_IO_DCAM_SWITCH
+		|| cmd == SPRD_IMG_IO_CFG_PARAM_STATUS) {
 		mutex_lock(&module->lock);
 		locked = 1;
 	}
