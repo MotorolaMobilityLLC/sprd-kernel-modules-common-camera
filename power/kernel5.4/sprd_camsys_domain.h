5633a3a96b292297c77c61ba94e4fa0b0a6930ca
bin.chen1 <bin.chen1@unisoc.com>
2022-07-06 06:01:13 +0000

    Bug #1941477modify license in power domain
    [root cause  ]: wrong license in power domain
    [changes     ]:update correct license
    [side effects]:no
    [self test   ]:yes
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:no
    [board]:L3 L5PRO L6 L6PRO
    [test case]:camera test
    [reviewers]:eddy.wei
    commit_template_version:v1
    [change_type ] feature_bugfix
    [tag_product ] specific
    
    Change-Id: I2e472403141fe1f1debab743906d59966db5c086

--- power/kernel5.4/sprd_camsys_domain.h
+++ power/kernel5.4/sprd_camsys_domain.h
@@ -1,11 +1,15 @@
-/* SPDX-License-Identifier: GPL-2.0 */
-
 /*
- * Spreatrum camera pd driver header
- * Copyright (C) 2021 Spreadtrum, Inc.
- * Author: Hongjian Wang <hongjian.wang@spreadtrum.com>
+ * Copyright (C) 2021-2022 UNISOC Communications Inc.
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  */
-
 
 #ifndef SPRD_CAMSYS_DOMAIN_H
 #define SPRD_CAMSYS_DOMAIN_H
--- power/kernel5.4/sprd_camsys_pw_domain_drv.c
+++ power/kernel5.4/sprd_camsys_pw_domain_drv.c
@@ -1,9 +1,15 @@
-// SPDX-License-Identifier: GPL-2.0
-//
-// Spreatrum UMS512 camera pd driver
-//
-// Copyright (C) 2018 Spreadtrum, Inc.
-// Author: Hongjian Wang <hongjian.wang@spreadtrum.com>
+/*
+ * Copyright (C) 2021-2022 UNISOC Communications Inc.
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
 
 #include <linux/kernel.h>
 #include <linux/module.h>
