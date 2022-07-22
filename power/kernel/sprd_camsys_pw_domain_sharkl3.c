eaf122f056e298bcd67460f73970f59a08188a04
Mengcheng Wang <mengcheng.wang@unisoc.com>
2022-07-22 20:00:52 +0800

    Bug #1961399 bsp: camera use sprdboot.mode for cali mode check
    [root cause  ]: bootconfig keyword change on k5.15
    [changes     ]: use sprdboot.mode
    [side effects]: no
    [self test   ]: yes
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:no
    [board]:ums9620
    [test case]:camera
    [reviewers]:huizi.gao
    
    commit_template_version:v1
    [change_type ] feature_bugfix
    [tag_product ] common
    
    Change-Id: I5b001ff347bdda1736b76068b73dfaafa3bac768
    Signed-off-by: Mengcheng Wang <mengcheng.wang@unisoc.com>

--- power/kernel/sprd_camsys_pw_domain_sharkl3.c
+++ power/kernel/sprd_camsys_pw_domain_sharkl3.c
@@ -63,7 +63,8 @@ static int boot_mode_check(void)
 	if (ret < 0)
 		return 0;
 
-	if (strstr(cmd_line, "androidboot.mode=cali"))
+	if (strstr(cmd_line, "androidboot.mode=cali") ||
+			strstr(cmd_line, "sprdboot.mode=cali"))
 		ret = 1;
 
 	return ret;
