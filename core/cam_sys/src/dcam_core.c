09b90f1672bc8da3087d6c8d85c9e3448328ed44
fei.hu <fei.hu@unisoc.com>
2022-11-01 14:32:35 +0800

    Bug #2063883 bsp: camera: Fix N6p k515 sysdump issue
    [root cause  ]:dcam irq proc thread not stop when creat success in low
    memory scene
    [changes     ]:camdrv
    [side effects]:NA
    [self test   ]:NA
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:no
    [board]:ums9620_k515
    [test case]:camera function
    [reviewers]:yangyang.liu
    commit_template_version:v1
    [change_type ] feature_bugfix
    [tag_product ] specific
    
    Fix N6p k515 kernel crash for dcam irq thread not stop when creat
    successin low memory scene.
    
    Change-Id: Ia0a3d36aebb27892f0ca14439990cd280742c3d3

--- core/cam_sys/src/dcam_core.c
+++ core/cam_sys/src/dcam_core.c
@@ -149,7 +149,7 @@ static int dcamcore_irq_proc(void *handle)
 
 static int dcamcore_context_init(struct dcam_pipe_dev *dev)
 {
-	int i = 0, ret = 0;
+	int i = 0, j = 0, ret = 0;
 	struct dcam_hw_context *pctx_hw = NULL;
 	struct cam_thread_info *thrd = NULL;
 
@@ -179,7 +179,8 @@ static int dcamcore_context_init(struct dcam_pipe_dev *dev)
 		ret = camthread_create(pctx_hw, thrd, dcamcore_irq_proc);
 		if (unlikely(ret != 0)) {
 			pr_err("fail to create dcam online interruption_proc thread\n");
-			return -EINVAL;
+			ret = -EINVAL;
+			goto thread_creat_fail;
 		}
 
 		pr_debug("register irq for dcam %d. irq_no %d\n", i, dev->hw->ip_dcam[i]->irq_no);
@@ -191,6 +192,18 @@ static int dcamcore_context_init(struct dcam_pipe_dev *dev)
 	}
 
 	pr_debug("done!\n");
+	return ret;
+
+thread_creat_fail:
+	cam_queue_clear(&pctx_hw->dcam_irq_sts_q, struct camera_frame, list);
+	for (j = 0; j < i; j++) {
+		pctx_hw = &dev->hw_ctx[j];
+		dcamcore_put_fmcu(pctx_hw);
+		dcam_int_irq_free(&dev->hw->pdev->dev, pctx_hw);
+		atomic_set(&pctx_hw->user_cnt, 0);
+		camthread_stop(&pctx_hw->dcam_irq_proc_thrd);
+		cam_queue_clear(&pctx_hw->dcam_irq_sts_q, struct camera_interrupt, list);
+	}
 	return ret;
 }
 
@@ -235,7 +248,7 @@ static int dcamcore_dev_open(void *dcam_handle)
 		ret = dcam_drv_hw_init(dev);
 		if (ret) {
 			pr_err("fail to init hw. ret: %d\n", ret);
-			goto context_init_fail;
+			goto exit;
 		}
 		dev->hw->dcam_ioctl(dev->hw, DCAM_HW_CFG_INIT_AXI, &hw_ctx_id);
 
@@ -243,7 +256,7 @@ static int dcamcore_dev_open(void *dcam_handle)
 		if (ret) {
 			pr_err("fail to init dcam context.\n");
 			ret = -EFAULT;
-			goto exit;
+			goto hw_init_fail;
 		}
 
 		mutex_init(&dev->path_mutex);
@@ -253,8 +266,9 @@ static int dcamcore_dev_open(void *dcam_handle)
 	pr_info("open dcam pipe dev done! enable %d\n",atomic_read(&dev->enable));
 	return 0;
 
-context_init_fail:
-	dcamcore_context_deinit(dev);
+hw_init_fail:
+	dcam_drv_hw_deinit(dev);
+
 exit:
 	atomic_dec(&dev->enable);
 	mutex_unlock(&dev->ctx_mutex);
