f16d22aa0617278a0ab76629ef9fd497aab0fb29
Payne Zhang <payne.zhang@unisoc.com>
2023-04-12 14:06:54 +0800

    Bug #2215496 add offline cap interface
    [root cause  ]:add offline cap interface
    [changes     ]:libcamera
    [side effects]:no
    [self test   ]:yes
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:no
    [board]:ums9230.
    [test case]:test camera
    [reviewers]:jianping.wang
    
    commit_template_version:v1
    
    Signed-off-by: Payne Zhang <payne.zhang@unisoc.com>
    [change_type ] feature_bugfix
    [tag_product ] common
    
    Change-Id: I1f9d002d6e8eb220b8e20fc94643f6db65061077
    (cherry picked from commit b7fdbf2a8e2d6d7c3fdd9a0538df88694dcb09a1)

--- interface/sprd_camsys.h
+++ interface/sprd_camsys.h
@@ -554,6 +554,18 @@ struct sprd_irq_info {
 	uint32_t frame_id;
 };
 
+struct offline_cap_param {
+    uint32_t frame_id;
+    uint32_t reserved;
+};
+
+struct sprd_offline_info {
+	uint32_t offline_type;
+	uint32_t offline_bpc_flag;
+	uint32_t offline_aisfnr;
+	struct offline_cap_param offline_param;
+};
+
 struct sprd_vir_ch_info {
 	uint32_t fd_array[IMG_PATH_BUFFER_COUNT];
 	enum dev_out_fmt dump_isp_out_fmt;
