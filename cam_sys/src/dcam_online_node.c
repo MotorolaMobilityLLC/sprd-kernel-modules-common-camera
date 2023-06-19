de1ff773a20dfc8a970b9279a54a9bfdf03b79d7
aobing.qi <Aobing.Qi@uniso.com>
2023-06-19 19:56:20 +0800

    Bug #2296513 add gtmhist bypass in dcamonline_dev_start
    [root cause  ]: there is need statis in highlight ultra res,but the
    bypass of gtmhist is set for 0,which will get gtmhist port and fail to
    [changes     ]:add gtmhist bypass in dcamonline_dev_start
    [side effects]:no
    [self test   ]:yes
    [download normally]:yes
    [power on/off normally]:yes
    [do common repository/branch inspection]:yes
    [is there dependence]:no
    [confirm dependent commit]:no
    [board]:all project
    [test case]:camera func
    [reviewers]:yangyang.liu
    
    commit_template_version:v1
    [change_type ] debug_log
    [tag_product ] common
    
    Change-Id: I7439862014ac4b861c3fd42e5fdf227fcf30face

--- cam_sys/src/dcam_online_node.c
+++ cam_sys/src/dcam_online_node.c
@@ -1385,8 +1385,10 @@ static int dcamonline_dev_start(struct dcam_online_node *node, void *param)
 	if (hw->ip_isp->isphw_abt->frbg_hist_support)
 		DCAMONLINE_STATIS_WORK_SET(pm->hist_roi.hist_roi_info.bypass, node, PORT_FRGB_HIST_OUT);
 	DCAMONLINE_STATIS_WORK_SET(pm->pdaf.bypass, node, PORT_PDAF_OUT);
-	if (hw_ctx->hw->ip_dcam[node->hw_ctx_id]->dcamhw_abt && hw_ctx->hw->ip_dcam[node->hw_ctx_id]->dcamhw_abt->rgb_gtm_support == CAM_ENABLE)
-		DCAMONLINE_STATIS_WORK_SET(0, node, PORT_GTM_HIST_OUT);
+	if (hw_ctx->hw->ip_dcam[node->hw_ctx_id]->dcamhw_abt && hw_ctx->hw->ip_dcam[node->hw_ctx_id]->dcamhw_abt->rgb_gtm_support == CAM_ENABLE) {
+		if (!pm->rgb_gtm.rgb_gtm_info.bypass_info.gtm_hist_stat_bypass || !pm->gtm.gtm_info.bypass_info.gtm_hist_stat_bypass)
+			DCAMONLINE_STATIS_WORK_SET(0, node, PORT_GTM_HIST_OUT);
+	}
 
 	hw_ctx->fid = pm->recovery_fid ? pm->recovery_fid : 0;
 	hw_ctx->recovery_fid = pm->recovery_fid;
